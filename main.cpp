26111f0778f1568cfbb4e80447dde91c9cb57e3e
Marco Carta-Gullung <mcarta-gullung@localhost.localdomain>
2015-12-05 01:12:56 +0100

    v1.0_alpha no libs
    Signed-off-by: Marco Carta-Gullung <mcarta-gullung@localhost.localdomain>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
-﻿#include <iostream>
+﻿/*
+ * Copyright (C) 2015, Marco Carta-Gullung, GroupeAntipolis.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
 using namespace std;
 
 #include "graph_manager.h"
@@ -44,7 +71,7 @@ int main (int argc, char *argv[])
 		//output[0]=inputs[0]*cos(inputs[1]/inputs[0]);
 		//output[0]=inputs[0]*inputs[1];
 
-		cout << t << "." << inputs[0] << "*" << inputs[1] << "+" << inputs[0] << "=" << output[0] << endl;
+		cout << "training" << t << ":  " << inputs[2] << " * " << inputs[3] << " * cos( " << inputs[1] << " / " << inputs[0] << " ) = " << inputs[2]*inputs[3]*cos(inputs[1]/inputs[0]) << endl;
 
 		mgm->add_training(inputs,output);
 	}
@@ -77,12 +104,14 @@ int main (int argc, char *argv[])
 		inputs[1]=(g_nbr)rnd.next_double();
 		inputs[2]=(g_nbr)rnd.next_double();
 		inputs[3]=(g_nbr)rnd.next_double();
-		res=inputs[2]*inputs[3]*cos(inputs[1]/inputs[0]);
+
+		cout << "ground(" << t << "):  " << inputs[2] << " * " << inputs[3] << " * cos( " << inputs[1] << " / " << inputs[0] << " ) = " << inputs[2]*inputs[3]*cos(inputs[1]/inputs[0]) << endl;
+
 		mgm->activate(inputs,output);
 		//output[0]=inputs[0]*cos(inputs[1]/inputs[0]);
 		//output[0]=inputs[0]*inputs[1];
 		
-		cout << t << "." << inputs[0] << "*" << inputs[1] << "+" << inputs[0] << "=" << res << " g=" << output[0] << endl;
+		cout << "graph(" << t << "):  " << inputs[2] << " * " << inputs[3] << " * cos( " << inputs[1] << " / " << inputs[0] << " ) = " << output[0] << endl << endl;
 		
 		mgm->add_training(inputs,output);
 	}
