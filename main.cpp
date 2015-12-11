b4e212b6028af2ba537250f45a39099a87e72bc6
Marco Carta-Gullung <mcarta@localhost.localdomain>
2015-12-11 03:03:03 +0100

    Added a lot of cool things (tests and guidance)
    Signed-off-by: Marco Carta-Gullung <mcarta@localhost.localdomain>

--- main.cpp
+++ main.cpp
@@ -30,10 +30,59 @@ using namespace std;
 
 #include "graph_manager.h"
 #include "random_gen.h"
-#include <unistd.h>
-#include "math_nodes.h"
+#include <unistd.h> //STDIN_FILENO
+#include "math_nodes.h" // involved in maths tests (I)
+#include "logical_nodes.h"  // involved in logical tests(II)
 #include "math.h"
 #include <fenv.h>
+#include <stdio.h>
+#include <termios.h>    	//termios, TCSANOW, ECHO, ICANON	
+
+
+void pressKey()
+{
+    //the struct termios stores all kinds of flags which can manipulate the I/O Interface
+    //I have an old one to save the old settings and a new 
+    static struct termios oldt, newt;
+    printf("Press key to continue....\n");
+
+    //tcgetattr gets the parameters of the current terminal
+    //STDIN_FILENO will tell tcgetattr that it should write the settings
+    // of stdin to oldt
+    tcgetattr( STDIN_FILENO, &oldt);
+    //now the settings will be copied 
+    newt = oldt;
+
+    //two of the c_lflag will be turned off
+    //ECHO which is responsible for displaying the input of the user in the terminal
+    //ICANON is the essential one! Normally this takes care that one line at a time will be processed
+    //that means it will return if it sees a "\n" or an EOF or an EOL
+    newt.c_lflag &= ~(ICANON | ECHO );  	
+
+    //Those new settings will be set to STDIN
+    //TCSANOW tells tcsetattr to change attributes immediately. 
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+
+    //now the char wil be requested
+    getchar();
+
+    //the old settings will be written back to STDIN
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+
+}
+
+void manager_train(graph_manager* mg)
+{
+	mg->start_training();
+
+	for(int t=0;t<100;t++)
+	{
+		sleep(30);
+		/**/if(mg->getScore()==0)
+		break;
+	} 
+	mg->stop_training();
+}
 
 int main (int argc, char *argv[])
 {
@@ -43,15 +92,30 @@ int main (int argc, char *argv[])
                    FE_OVERFLOW  | 
                    FE_UNDERFLOW);*/
 
+	// if u comment this one, glue can't do algebra (it can't work at all ;) )
 	math_lib_registration();
+	// if u comment this test II will never be passed ... (only approximated)
+	logic_lib_registration();
+	
+	// Our rnd generator (see sources in (include))
 	random_gen rnd;
 
-	cout << "Test de GLUE!" << endl;
+	cout << "Glue Test Routine !" << endl;
+	cout << "This will test a glue graph in diffrent tasks: " << endl;
+	pressKey();
 
+	int ch=0;
 	// we declare a graph object !!
+	
+	cout << "Test I, Maths : Glue should discover a math function on the form :" << endl;
+	cout << "E(a,b,c,d) = (c * d * cos( b /a ))" << endl << endl;
 	cout << "declare a graph manager object .." << endl;
-	cout << "created graph will have 2 in & 1 out .." << endl;
-	graph_manager* mgm=new graph_manager(4,1,5,200);
+	cout << "created graph will have 4 in & 1 out .." << endl;
+	
+	pressKey();
+	
+	//graph_manager(int pin,int pout,int min_nodes,int max_nodes);
+	graph_manager* mgm=new graph_manager(4,1,5,50);
 
 
 	mgm->test_geometer();
@@ -84,20 +148,16 @@ int main (int argc, char *argv[])
 
 	// start the training for 20 sec
 
-
-	mgm->start_training();
-
-	for(int t=0;t<100;t++)
-	{
-		sleep(30);
-		/**/if(mgm->getScore()==0)
-		break;
-	} 
-	mgm->stop_training();
+	cout << "Test I, Training: Glue will be trained until it can solve it." << endl;
 	
-	cout << "we test 10 queries .." << endl;
-	// we build 10 trainings
-	g_nbr res;
+	pressKey();
+	
+	manager_train(mgm);
+	
+	cout << "" << endl;
+	cout << "Test I, Test Graph: we test 10 queries ..." << endl;
+	
+
 	for(int t=0;t<10;t++)
 	{
 		inputs[0]=(g_nbr)rnd.next_double();
@@ -113,10 +173,87 @@ int main (int argc, char *argv[])
 		
 		cout << "graph(" << t << "):  " << inputs[2] << " * " << inputs[3] << " * cos( " << inputs[1] << " / " << inputs[0] << " ) = " << output[0] << endl << endl;
 		
-		mgm->add_training(inputs,output);
 	}
 
 	delete(mgm);
+	
+		// we declare a graph object !!
+	
+	cout << endl << endl << "Test II, Logic : Glue should discover the INDEX of its INPUT having non zero value :" << endl;
+	cout << endl;
+	cout << "declare a graph manager object .." << endl;
+	cout << "created graph will have 4 in & 1 out .." << endl;
+	
+	pressKey();
+	
+	mgm=new graph_manager(4,1,5,50);
+
+	cout << "we build 100 trainings .." << endl;
+	// we build 10 trainings
+	for(int t=0;t<100;t++)
+	{
+		inputs[0]=0; //(g_nbr)rnd.next_double();
+		inputs[1]=0;
+		inputs[2]=0;
+		inputs[3]=0;
+		
+		ch=rnd.next(4);
+		
+		inputs[ch]=(g_nbr)rnd.next_double();
+		//inputs[ch]=1;
+		
+		
+		cout << "Non-zero index is " <<  ch << endl;
+
+		output[0]=ch;
+		
+
+		mgm->add_training(inputs,output);
+	}
+
+	cout << "manager have " << mgm->get_training_count() << " trainings .." << endl;/**/
+
+	// test some things
+
+	/*mgm->test_glue();*/
+
+	// start the training for 20 sec
+
+	cout << "Test II, Training: Glue will be trained until it can solve it." << endl;
+	
+	pressKey();
+	
+	manager_train(mgm);
+	
+	cout << "" << endl;
+	cout << "Test II, Test Graph: we test 10 queries ..." << endl;
+	
+	g_nbr res;
+	for(int t=0;t<10;t++)
+	{
+		inputs[0]=0; //(g_nbr)rnd.next_double();
+		inputs[1]=0;
+		inputs[2]=0;
+		inputs[3]=0;
+		
+		ch=rnd.next(4);
+		
+		inputs[ch]=(g_nbr)rnd.next_double();
+		//inputs[ch]=1;
+		
+		
+		cout << "Non-zero index is " <<  ch << endl;
+
+		mgm->activate(inputs,output);
+		//output[0]=inputs[0]*cos(inputs[1]/inputs[0]);
+		//output[0]=inputs[0]*inputs[1];
+		
+		cout << "Predicted non-zero index is " <<  output[0] << endl;
+		
+	}
+
+	delete(mgm);
+	
 	return 0;
 };
 
--- main.d
+++ main.d
@@ -2,4 +2,4 @@ main.o: main.cpp include/graph_manager.h include/graph.h include/node.h \
  include/glue_types.h include/random_gen.h include/mutation_stack.h \
  include/global.h include/node_factory.h include/graph_coach.h \
  include/graph_evaluators.h include/graph_player.h include/random_gen.h \
- include/math_nodes.h
+ include/math_nodes.h include/logical_nodes.h include/math_nodes.h
