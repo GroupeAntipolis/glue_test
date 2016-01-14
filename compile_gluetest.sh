d0aa26aa46fe529c434a59b70f1201530a814833
Arnaud TANGUY <arn.tanguy@gmail.com>
2016-01-14 02:00:39 +0100

    Add CMakeLists

--- compile_gluetest.sh
+++ compile_gluetest.sh
@@ -9,5 +9,8 @@ g++  -MMD main.cpp -O3 -Iinclude -Iobj/Release/prec/Release  -c -o main.o
 # Generation du binaire "gluetest" depuis les fichiers objets.
 g++ -o bin/Release/gluetest main.o  libglue/libglue.a libglue/libSmartData.a libglue/librng.a -lpthread
 
+rm main.o
+rm main.d
+
 # Finalisation
 chmod  755 bin/Release/gluetest
